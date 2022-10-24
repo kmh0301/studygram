587670ccd068afdd35fa37381fb3130b34f2e6d0
kmh0301 <komanhei0301@gmail.com>
2022-10-24 19:10:11 +0800

    testing

--- app/src/main/java/com/example/studygram/MainActivity.java
+++ app/src/main/java/com/example/studygram/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //testing kmhhhh
+//testing kmhhhh
+//testing kmhhhh
+//testing kmhhhh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
