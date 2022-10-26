c8acad8b4277f7a70ceba1a3302f6f6313a3573c
Sally <81373766+sallypang626@users.noreply.github.com>
2022-10-26 16:12:27 +0800

    test

--- app/src/main/java/com/example/studygram/MainActivity.java
+++ app/src/main/java/com/example/studygram/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.studygram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
+    private Button mBtnloginpage;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnloginpage = findViewById(R.id.btb_login);
+        mBtnloginpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
